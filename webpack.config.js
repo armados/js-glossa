e9ad758f28958a29781084cf0b26d4a3d375db30
Nikos Zounis <armados@gmail.com>
2020-10-02 20:36:09 +0300

    pagona

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,17 @@ const path = require("path");
 
 module.exports = {
     entry: {
-        glossajs: './app.js'
+        glossajs: './app.js',
     },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'glossajs.js',
         library: 'GlossaJS'
     },
+
+    externals: {
+        "fs": "require('fs')",
+        "path": "require('path')"
+    }
+  
 }

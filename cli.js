bab7bb200ae8aad2b33be2d3b67b6ccfd88736ce
Nikos Zounis <armados@gmail.com>
2020-10-04 16:31:58 +0300

    update cli script

--- cli.js
+++ cli.js
@@ -2,28 +2,32 @@
 
 "use strict";
 
-var GlossaJS   = require("../app");
+var GlossaJS = require("../app");
 
 var fs = require("fs");
-var minimist = require('minimist');
+var minimist = require("minimist");
 
-//FIXME: 
+//FIXME:
 var args = minimist(process.argv.slice(2), {
-  string: ['input'],           // --lang xml
-  boolean: ['version'],     // --version
-  alias: { v: 'version', i: 'input' },
-  default: {  },
-  stopEarly: true, /* populate _ with first non-option */
-  unknown: function () { throw new GE.GError('Invalid arguments'); }
+  string: ["input"], // --lang xml
+  string: ["keyboard"], // --lang xml
+  boolean: ["version"], // --version
+  alias: { v: "version", i: "input", k: "keyboard" },
+  default: {},
+  stopEarly: true /* populate _ with first non-option */,
+  unknown: function () {
+    throw new GE.GError("Invalid arguments");
+  },
 });
 
-if (!(args['input']))
-  throw new GE.GError('Missing input file');
+if (!args["input"]) throw new GE.GError("Missing input file");
 
-var filename = args['input'];
-var sourceCode = fs.readFileSync( filename ).toString();
+var filename = args["input"];
+var sourceCode = fs.readFileSync(filename).toString();
 
 var inputKeyboardBuffer = null;
+if (args["keyboard"]) inputKeyboardBuffer = args["keyboard"];
+
 var output = GlossaJS.parseGlossaJS(sourceCode, inputKeyboardBuffer);
 
 console.log(output);
